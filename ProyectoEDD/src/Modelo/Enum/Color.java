489c640b28a87969a3e69a99c53a41672be45ab3
DawnNF721 <87239896+DawnNF721@users.noreply.github.com>
2021-12-27 13:31:10 -0500

    Add files via upload

---
+++ ProyectoEDD/src/Modelo/Enum/Color.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Enum;
+
+/**
+ *
+ * @author Alba
+ */
+public enum Color {
+    ROJO,
+    AZUL,
+    NEGRO,
+    BLANCO,
+    CAFE,
+    AMARILLO,
+    GRIS,
+    VERDE
+}
