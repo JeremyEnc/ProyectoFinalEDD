d0656bb487f2cb6ab694a404842175a79b53a662
JeremyEnc <92940766+JeremyEnc@users.noreply.github.com>
2021-12-28 10:32:06 -0500

    Update ControladorFacturaCompra.java

--- ProyectoEDD/src/Controlador/ControladorFacturaCompra.java
+++ ProyectoEDD/src/Controlador/ControladorFacturaCompra.java
@@ -1,12 +1,11 @@
 /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Controlador;
 
-import Controlador.Dao.AdaptadorDao;
-import Controlador.EstructurasDinamicas.Lista;
 import Modelo.DetalleCompra;
 import Modelo.FacturaCompra;
 import Modelo.Proveedor;
@@ -15,13 +14,68 @@ import Modelo.Proveedor;
  *
  * @author jere_
  */
-public class ControladorFacturaCompra extends AdaptadorDao<FacturaCompra> 
+public class ControladorFacturaCompra 
 {
-    Lista<DetalleCompra> listaDetallesCompra;
+    FacturaCompra factura;
     Proveedor proveedor;
-
-    public ControladorFacturaCompra()
-    {
-        super(FacturaCompra.class);
+    
+    public FacturaCompra getFactura() {
+        return factura;
     }
+
+    public void setFactura(FacturaCompra factura) {
+        this.factura = factura;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public void generarNumeroFactura()
+    {
+        String nroFact = Integer.toString((int)(Math.random()*10000000));
+        
+        factura.setNroFactura(nroFact);
+        
+    }
+    
+    public void calcularSubtotal()
+    {
+        double subTotal = 0;
+        
+        for (int i = 0; i < factura.getListaDetalles().length(); i++)
+        {
+           subTotal+= factura.getListaDetalles().getByIndex(i).getPrecioTotal();
+        }
+        
+        factura.setSubTotal(subTotal);
+    }
+    
+    public void calcularIva()
+    {
+        double precioIva = factura.getSubTotal() * 0.12;
+        factura.setIva(precioIva);
+    }
+    
+    public void calcularTotal()
+    {
+        double precioFinal = factura.getIva() + factura.getSubTotal();
+        
+        factura.setTotal(precioFinal);
+    }
+    
+    public void agregarDetalleCompra(Long idProducto, Integer cantidad, Double precio )
+    {
+        long id = factura.getListaDetalles().length() + 1;
+        
+        DetalleCompra detCompra = new DetalleCompra(id, factura.getId(), idProducto, cantidad, precio);
+        
+        factura.getListaDetalles().add(detCompra);
+    }
+    
+    
 }
