d7c6c4cbe23b001e7412688ac6f6e4eb04f2049c
Santiago-ARG <83770659+Santiago-ARG@users.noreply.github.com>
2021-12-28 11:27:02 -0500

    Add files via upload

---
+++ ProyectoEDD/src/Modelo/Persona.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Controlador.EstructurasDinamicas.Lista;
+
+/**
+ *
+ * @author santiago-arg
+ */
+public class Persona {
+
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private Long id_Rol;
+    private String direccion;
+    private String correo;
+    private String identificacion;
+    private Lista<Factura> Facturas;
+
+    public Lista<Factura> getFacturas() {
+        return Facturas;
+    }
+
+    public void setFacturas(Lista<Factura> Facturas) {
+        this.Facturas = Facturas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getId_Rol() {
+        return id_Rol;
+    }
+
+    public void setId_Rol(Long id_Rol) {
+        this.id_Rol = id_Rol;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+}
