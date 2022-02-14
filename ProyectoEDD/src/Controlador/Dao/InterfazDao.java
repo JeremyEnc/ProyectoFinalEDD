d05db37d1e5ff0ee4fcbbb7da33cd07628f20898
JeremyEnc <92940766+JeremyEnc@users.noreply.github.com>
2022-02-13 22:15:40 -0500

    Update InterfazDao.java

--- ProyectoEDD/src/Controlador/Dao/InterfazDao.java
+++ ProyectoEDD/src/Controlador/Dao/InterfazDao.java
@@ -10,12 +10,27 @@ import Controlador.EstructurasDinamicas.Lista;
 /**
  *
  * @author jere_
- * @param <T>
+ * @param <T> es la clase que se guardara en archivos
  */
 public interface InterfazDao <T>
 {
+    /**
+     * Metodo para guardar el objeto de tipo T en archivos
+     * @param dato el objeto que se va a guardar
+     * @return Verdadero si se guardo sin ningun problema, falso si hubo algun problema
+     */
     public boolean guardar(T dato);
+    /**
+     * Metodo para modificar algun elemento que se haya guardaro en el sistema
+     * @param dato El nuevo objeto que va a remplazar al anterior
+     * @param pos La posicion que va a remplazar
+     * @return  Verdadero si se guardo sin ningun problema, falso si hubo algun problema
+     */
     public boolean modificar(T dato, int pos);
+    /**
+     * Metodo para listar los objetos que se encuentren guardaros en el archivo
+     * @return la lista de objetos guardados en el archivo
+     */
     public Lista<T> listar();
     
     
