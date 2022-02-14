8269eee8359bd6c0a2d47a6eb33bb1b9ca9a80e7
DawnNF721 <87239896+DawnNF721@users.noreply.github.com>
2022-02-13 21:32:02 -0500

    Add files via upload

---
+++ ProyectoEDD/src/Modelo/Enum/Color.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Enum;
+
+/**
+ *
+ * @author Alba
+ */
+public enum Color {
+    //Colores en los que se puede encontrar la ropa
+    ROJO,
+    AZUL,
+    NEGRO,
+    BLANCO,
+    CAFE,
+    AMARILLO,
+    GRIS,
+    VERDE
+}
---
+++ ProyectoEDD/src/Modelo/Enum/Talla.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Enum;
+
+/**
+ *
+ * @author Alba
+ */
+public enum Talla {
+    //Tallas en las que se puede encontrar la ropa
+    XS,
+    S,
+    M,
+    L,
+    XL
+}
